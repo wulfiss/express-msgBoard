21be59e6a33f7947da1ff73302543cbf03ddcb81
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-07-25 18:26:22 -0300

    Add messages

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
+const messages = [
+  {
+   text: 'Hi there!',
+   user: 'Juan de los palotes',
+   added: new Date() 
+  },
+  {
+    text: 'Hello World!',
+    user: 'Wulfis the First',
+    added: new Date()
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/new', (req, res, next) => {
-  res.render('index', { title: 'Wulfis'});
-})
 
 module.exports = router;
